35ebe781b5387b3f30eed52a85f17d61fc5baee6
Nguyenbakhoa-sacly <khoana23062002@gmail.com>
2024-04-07 21:01:22 +0700

    Divide page layout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
+import CssBaseline from '@mui/material/CssBaseline'
+import Theme from './theme.js'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CssVarsProvider theme={Theme}>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>,
 )
